b357e2f8bcd3d992224a51319a66fb72c54f5386
Nishant Kumar Gupta <nishantkumargupta728@gmail.com>
2025-07-09 18:29:15 +0530

    add some texting

--- src/app/components/Understand.jsx
+++ src/app/components/Understand.jsx
@@ -26,25 +26,25 @@ export default function AyurvedaSection() {
         viewport={{ once: false }}
         className="w-full max-w-6xl flex flex-col items-center text-center space-y-8"
       >
-        {/* Title */}vv
+        {/* Title */}
         <h1 className="text-Black-Mango-ExtraBold text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-snug font-normal">
           Understanding Ayurveda: The Sacred <br /> Science of Balance
         </h1>
 
         {/* Subheading Paragraph */}
-        <p className="text-white font-light leading-relaxed max-w-4xl text-base sm:text-lg md:text-xl">
+        <p className="text-gtwalsheim text-white font-light leading-relaxed max-w-4xl text-base sm:text-lg md:text-xl">
           Derived from the Sanskrit words āyuḥ (life) and veda (knowledge), Ayurveda is the comprehensive study of
           life itself — encompassing not only the physical body, but also the mind, senses, emotions, and spiritual
           constitution.
         </p>
 
         {/* Key principles title */}
-        <h2 className="text-white font-semibold text-xl sm:text-2xl md:text-2xl">
+        <h2 className="text-white font-semibold text-xl sm:text-2xl text-gtwalsheim md:text-2xl">
           Key principles include:
         </h2>
 
         {/* Key Bullet Points */}
-        <div className="text-white font-light text-base sm:text-lg md:text-xl leading-relaxed space-y-2 max-w-4xl">
+        <div className="text-gtwalsheim text-white font-light text-base sm:text-lg md:text-xl leading-relaxed space-y-2 max-w-4xl">
           <p>
             The Three Doṣas: Vāta, Pitta, and Kapha — elemental energies that govern bodily function and mental tendencies.
           </p>
